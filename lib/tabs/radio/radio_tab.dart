e78967b20f3d2efe3c00a4c44bd9b476596825a1
Mahmoud Gabr <mahmoudgabr1121@gmail.com>
2024-07-26 16:46:56 +0300

    create radio tab UI

--- lib/tabs/radio/radio_tab.dart
+++ lib/tabs/radio/radio_tab.dart
@@ -5,8 +5,58 @@ class RadioTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.black,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset('assets/images/radio_image.png'),
+        const SizedBox(
+          height: 57,
+        ),
+        Text(
+          'إذاعة القرأن الكريم',
+          style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                fontWeight: FontWeight.w600,
+              ),
+        ),
+        const SizedBox(
+          height: 57,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.skip_previous_rounded,
+                color: Theme.of(context).primaryColor,
+                size: 64,
+              ),
+            ),
+            const SizedBox(
+              width: 60,
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.play_arrow_rounded,
+                color: Theme.of(context).primaryColor,
+                size: 64,
+              ),
+            ),
+            const SizedBox(
+              width: 60,
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.skip_next_rounded,
+                color: Theme.of(context).primaryColor,
+                size: 64,
+              ),
+            ),
+          ],
+        )
+      ],
     );
   }
 }
